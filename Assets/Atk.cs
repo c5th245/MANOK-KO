4f902fb19829343a52f985c011e6f77e6bdf48ae
mark <tontonmark12@gmail.com>
2026-02-08 17:22:02 +0800

    character
    added sprite and animation for lapu lapu it has its own folder

--- Assets/Atk.cs
+++ Assets/Atk.cs
@@ -6,20 +6,5 @@ public class PlayerAttack : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            swordHitbox.SetActive(true);
-            Invoke(nameof(StopAttack), 0.2f);
-        }
     }
-
-    void StopAttack()
-    {
-        swordHitbox.SetActive(false);
-    }
-    void OnTriggerEnter2D(Collider2D other)
-{
-    if (other.CompareTag("Enemy"))
-        other.GetComponent<Enemy>().TakeDamage(1);
-}
 }
